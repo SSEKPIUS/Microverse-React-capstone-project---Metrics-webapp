5627953382ac00ad86a3acb8997a04e859bc5ad7
pius ssekweyama <ssekweyamapius@gmail.com>
2023-01-21 19:53:34 +0300

    check

--- src/components/Cities.js
+++ src/components/Cities.js
@@ -1,8 +1,10 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable max-len */
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import uniqid from 'uniqid';
 import { getZones } from '../redux/Zones';
 import imagesx from './images/Images';
-import CitiesList from './CitiesList';
 
 const Cities = () => {
   const { zones = [] } = useSelector((state) => state.zones);
@@ -28,6 +30,23 @@ const Cities = () => {
     18: 'Mumbai',
     19: 'Johannesburg',
   };
+
+  const shade = [false, 0];
+  let start = true;
+  const getShade = () => {
+    if (start) {
+      start = false;
+      return 'striped';
+    }
+    if (shade[1] > 1) {
+      shade[0] = !shade[0];
+      shade[1] = -1;
+    }
+    shade[0] = shade[1] > 1 ? !shade[0] : shade[0];
+    shade[1] += 1;
+    return shade[0] === true ? 'striped' : '';
+  };
+
   const images = imagesx();
   useEffect(() => {
     if (zones.length === 0) dispatch(getZones(cities[1]));
@@ -59,7 +78,38 @@ const Cities = () => {
           Zones BY Cities
           <span className=" font-thin ml-2">(Sroll Right)</span>
         </span>
-        <CitiesList zones={zones} />
+        <div className="flex flex-row flex-wrap gap-0 striped">
+          {
+            zones.map((zone) => (
+              <div key={(`${uniqid()}`)} className={`w-1/2 md:w-2/6 lg:w-1/4 h-52 relative p-5 ${getShade()}`}>
+                {/* <div
+                    style={{
+                    backgroundImage: `url("${img09}")`, backgroundRepeat: 'no-repeat', backgroundSize: 'contain', filter: 'blur(0px)',
+                    }}
+                    className=" w-full h-full"
+                />
+                <div
+                    className="absolute top-0 left-0 w-full h-full flex flex-col gap-1 items-end justify-end text-white pb-2"
+                    onClick={() => { navigate('/city', { state: { lat: zone.lat, lon: zone.lon, name: zone.name } }); }}
+                    aria-hidden="true"
+                >
+                    <span className=" font-extrabold text-xl text-gray-100 mr-2 shadow-sm drop-shadow-lg shadow-black">{zone.name}</span>
+                    <span className="mr-2">
+                    {zone.country}
+                    </span>
+                    <span className="mr-2">
+                    <span className=" text-black capitalize mr-2">lat:</span>
+                    {zone.lat}
+                    </span>
+                    <span className="mr-2">
+                    <span className=" text-black capitalize mr-2">lon:</span>
+                    {zone.lon}
+                    </span>
+                </div> */}
+              </div>
+            ))
+      }
+        </div>
       </div>
     </div>
   );
